05a0dd97c8d8bc1237ac49b40a410a2ba5eacfa1
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-04-22 17:31:01 +0200

    working with class in App

---
+++ readme.md
@@ -0,0 +1,5 @@
+# This course is based on react for beginners
+
+# @steps and configurations - project structures:
+
+1. npx create-react-app react-task-tracker
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className='container'>
+//       <Header />
+//     </div>
+//   );
+// }
+
+// with classe
+class App extends React.Component {
+  render() {
+    return <h1>Hello World, with class in React.</h1>;
+  }
 }
 
 export default App;
